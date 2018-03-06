81c99ab120fe3d70766ae3817c2c8dc56f8d7180
mkojoa <michaelameyaw7@gmail.com>
2018-03-06 01:58:51 +0000

    Init repo

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT =  process.env.PORT || 4000;
+
+app.get('/', function (req,res) {
+  res.send("To do api root")
+});
+
+app.listen(PORT ,function () {
+  console.log("Am on prt" + PORT);
+})
