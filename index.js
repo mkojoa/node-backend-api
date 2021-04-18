ff2270d473054e01df0333773742f579e6b391fc
Michael Ameyaw <michaelameyaw7@gmail.com>
2021-04-18 20:50:46 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// content hidden
+//all content hidden
 
 //lib
 const express = require('express');
@@ -9,6 +9,7 @@ app.get('/', function (req,res) {
   res.send("To do api root")
 });
 
+//server only
 app.listen(PORT ,function () {
   console.log("Am on prt" + PORT);
 })
